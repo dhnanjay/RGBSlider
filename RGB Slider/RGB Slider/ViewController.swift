179a326c3831f1c9b3820fa143372b9fb90d7185
DHANANJAY KUMAR <dhan980@gmail.com>
2015-04-02 20:58:00 -0400

    Project Added

---
+++ RGB Slider/RGB Slider/ViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  RGB Slider
+//
+//  Created by DHANANJAY KUMAR on 4/2/15.
+//  Copyright (c) 2015 Dhananjay Kumar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var redSlider: UISlider!
+    @IBOutlet weak var greenSlider: UISlider!
+    @IBOutlet weak var blueSlider: UISlider!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func sliderChanged(sender: UISlider) {
+       
+        if sender == redSlider {
+            println("Red Slider changed !")
+        }
+    }
+
+
+}
+
